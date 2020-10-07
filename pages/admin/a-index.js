545c2cbb6186889fd55aff9bbf3d1d747e7c1654
nickm0rash <71356183+nickm0rash@users.noreply.github.com>
2020-10-07 11:04:56 -0300

    Add files via upload

---
+++ pages/admin/a-index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function admin() {
+    return(
+      <div>Hello</div>  
+    )
+}
---
+++ pages/student/s-index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function student() {
+    return(
+      <div>Hello</div>   
+    )
+}
