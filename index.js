86d695f18f09780d13bf9b8eba542778da68627a
just1ngray <just1ngray@outlook.com>
2020-10-08 23:08:18 -0300

    implemented basic server functionality

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("TODO: Implement express backend server");
+/**
+ * Set up the Next & Express server.
+ * @author Justin Gray (A00426753)
+ */
+const express = require("express");
+const next = require("next");
+
+// @ts-ignore
+const app = next({
+  dir: "./client",
+});
+const handle = app.getRequestHandler();
+const port = 3384;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    // express middleware function(s)
+    server.use(express.json());
+
+    // startup functions
+    require("./startup/routes")(server); // setup '/api' routes
+    require("./startup/database").connect(); // connect to MongoDB
+
+    // render NextJS react content (generated using 'npm run build')
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    // set the server to listen on the given port
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
